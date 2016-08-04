6133fe60a3435a34ef180f3440816711565b475d
Tommy Ip <hkmp7tommy@gmail.com>
2016-08-04 17:22:43 +0800

    Restructured directories

--- test/test_numerical_method.py
+++ test/test_numerical_method.py
@@ -9,6 +9,12 @@ def test_interval_bisection():
     assert nm.interval_bisection([1, 0, 3, -12], 1, 2, 10)[0] == '1.85888907'
 
 
+def test_general_iteration():
+    assert nm.general_iteration_method([1, 0, -3, -4], 2, 8)[0] == '2.19582335'
+    assert nm.general_iteration_method([1, 0, 1, -6], 1, 8)[0] == '1.63436529'
+    assert nm.general_iteration_method([1, 0, 3, -12], 1, 8)[0] == '1.85888907'
+
+
 def test_newton_raphson():
     assert nm.newton_raphson_method([1, 0, -3, -4], 2, 8)[0] == '2.19582335'
     assert nm.newton_raphson_method([1, 0, 1, -6], 1, 8)[0] == '1.63436529'
